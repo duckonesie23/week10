d3dfdc6da5e17afc2d8d08cc7cddb6bb57711a16
mbondoc <maxbondoc11@gmail.com>
2024-04-15 14:30:58 -0400

    fasd

---
+++ AccountSetup.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class AccountSetup{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        String username = "";
+        String password = "";
+        System.out.print("Enter a user name: ");
+        username = input.next();
+        while(password.length()<=8){
+            System.out.print("Enter a password that is at least 8 characters: ");
+            password = input.next();
+        }
+        System.out.print("Your user name is "+username.toLowerCase()+" and your password is "+password.toLowerCase());
+    }
+}
